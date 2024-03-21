7721cf6c130112651df4e71196147a12110a0f2a
sacha18 <103282007+sacha18@users.noreply.github.com>
2024-03-21 16:50:43 +0100

    fix: Config/Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -45,6 +45,27 @@ class Database extends Config
         'numberNative' => false,
     ];
 
+    public array $production = [
+        'DSN'          => '',
+        'hostname'     => 'localhost',
+        'username'     => 'root',
+        'password'     => 'root',
+        'database'     => 'pizzasio_ci',
+        'DBDriver'     => 'MySQLi',
+        'DBPrefix'     => '',
+        'pConnect'     => false,
+        'DBDebug'      => true,
+        'charset'      => 'utf8',
+        'DBCollat'     => 'utf8_general_ci',
+        'swapPre'      => '',
+        'encrypt'      => false,
+        'compress'     => false,
+        'strictOn'     => false,
+        'failover'     => [],
+        'port'         => 3306,
+        'numberNative' => false,
+    ];
+
     /**
      * This database connection is used when
      * running PHPUnit database tests.
